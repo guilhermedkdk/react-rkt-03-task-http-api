32adacadf7e6bc205ef528a3000c428131995374
guilhermedkdk <guilhermeperesmm@gmail.com>
2025-02-20 14:00:46 -0300

    feat: fetch users on home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Banner } from "../../components/Banner";
 import { Input } from "../../components/Input";
+import { Loading } from "../../components/Loading";
+import { Card } from "../../components/Card";
+import { api } from "../../libs/axios";
 
-import { HomeContainer } from "./styles";
+import { Cards, HomeContainer } from "./styles";
+
+interface UserInfo {
+  avatarUrl: string;
+  login: string;
+}
 
 export function Home() {
   const [search, setSearch] = useState("");
+  const [users, setUsers] = useState<UserInfo[]>([]);
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [isFirstRender, setIsFirstRender] = useState(true);
+
+  async function fetchUsers() {
+    const { data } = await api.get(`/search/users?q=${search}`);
+
+    const users = data.items.map((user: any) => {
+      return {
+        avatarUrl: user.avatar_url,
+        login: user.login,
+      };
+    });
+
+    setUsers(users);
+    setIsLoadingUsers(false);
+  }
+
+  useEffect(() => {
+    if (isFirstRender) {
+      setIsFirstRender(false);
+    } else {
+      setIsLoadingUsers(true);
+
+      const timeout = setTimeout(async () => {
+        await fetchUsers();
+      }, 3000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [search]);
 
   return (
     <HomeContainer>
@@ -20,6 +59,17 @@ export function Home() {
         placeholder="Buscar conta"
         onChange={(event) => setSearch(event.target.value)}
       />
+
+      <Cards>
+        {isLoadingUsers ? (
+          <>
+            <Loading />
+            <Loading />
+          </>
+        ) : (
+          users.map((user) => <Card key={user.login} user={user} />)
+        )}
+      </Cards>
     </HomeContainer>
   );
 }
